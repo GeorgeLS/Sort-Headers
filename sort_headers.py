8e5772e2bfb2d68ef399945c6fe0ec400d1b35b5
GeorgeLS <sdi1600094@di.uoa.gr>
2018-05-28 12:53:08 +0300

    Optimized a bit the script

--- sort_headers.py
+++ sort_headers.py
@@ -2,27 +2,37 @@ import sys, fileinput
 
 headers = []
 
+def is_directive(_str):
+    return _str.find("#define") != -1 or _str.find("#pragma") != -1
+
+def extract_header(n, _str):
+    s_pos = 0
+    e_pos = 0
+    cur_pos = 0
+    for ch in _str:
+        cur_pos += 1;
+        if ch == '<' or ch == '"':
+            s_pos = cur_pos
+        if ch == '>' or ch == '"':
+            e_pos = cur_pos - 1
+    headers.append((n, _str[s_pos : e_pos]))
+
 def read_headers_from_file(filename):
     file = open(filename, "r")
     line_n = 0
     for line in file:
-        if line.find("#if") != -1:
+        if line.find("#include") != -1:
+            extract_header(line_n, line)
+        elif line.find("#if") != -1:
             for aux_line_n, aux_line in enumerate(file):
                 if aux_line.find("#endif") != -1:
                     break
             line_n += aux_line_n + 2
             continue
-        if line.find("#include") != -1:
-            s_pos = 0
-            e_pos = 0
-            cur_pos = 0
-            for ch in line:
-                cur_pos += 1;
-                if ch == '<' or ch == '"':
-                    s_pos = cur_pos
-                if ch == '>' or ch == '"':
-                    e_pos = cur_pos - 1
-            headers.append((line_n, line[s_pos : e_pos]))
+        elif is_directive(line) or line == "\n":
+            pass
+        else:
+            break
         line_n += 1
     file.close()
 
@@ -46,4 +56,3 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
